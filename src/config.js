bd5a3d12f853ba9a1b57b7a4220e875bd0a5d53b
Olie Chan <olie.chan@nearform.com>
2022-08-02 12:40:18 +0100

    feat: add hello-world bolt integration (#14)
    * feat: add hello-world bolt integration
    
    * chore: cleanup code
    
    * chore: add link to docs
    
    * chore: lint
    
    * refactor: remove envs not needed in this feat

---
+++ src/config.js
@@ -0,0 +1,17 @@
+import { join } from 'desm'
+import S from 'fluent-json-schema'
+import envSchema from 'env-schema'
+
+const schema = S.object()
+  .prop('SLACK_TOKEN', S.string().required())
+  .prop('SLACK_SIGNING_SECRET', S.string().required())
+  .prop('SLACK_CHANNEL', S.string().required())
+
+const config = envSchema({
+  schema,
+  dotenv: {
+    path: join(import.meta.url, '../.env'),
+  },
+})
+
+export default config
