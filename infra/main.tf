800e6a4238f523bacb397352ef0beb0442ece2a7
Kalil Mori Lopes <kalil.mori.lopes@nearform.com>
2022-08-08 07:56:37 -0300

    include simple sns topic to test iam role

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,9 @@
-module lambda {
-  source   = "./modules/lambda"
-  project  = var.project
+# module lambda {
+#   source   = "./modules/lambda"
+#   project  = var.project
+# }
+
+module sns {
+  source = "./modules/sns"
 }
+
