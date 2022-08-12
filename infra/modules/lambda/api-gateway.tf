f3a33e9d9e18f0897926b648dbd2f95f3a049e96
Kenan Dervisevic <kenan.dervisevic@nearform.com>
2022-08-12 13:22:23 +0200

    remove duplicate permission

--- infra/modules/lambda/api-gateway.tf
+++ infra/modules/lambda/api-gateway.tf
@@ -56,14 +56,3 @@ resource "aws_api_gateway_deployment" "apideploy" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
   stage_name  = "test"
 }
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.board_notification.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # The "/*/*" portion grants access from any method on any resource
-  # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.apiLambda.execution_arn}/*/*"
-}
