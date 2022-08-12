0ed0954a37dc1420dcd1b47f182d44f90618efe3
Ionuț Florescu Țicleanu <ionut.florescu-ticleanu@nearform.com>
2022-08-12 11:55:28 +0300

    fix: lambda handler fix

--- lambda.js
+++ lambda.js
@@ -1,9 +1,4 @@
 import awsLambdaFastify from '@fastify/aws-lambda'
 import init from './src/app.js'
 const app = init()
-const main = async () => {
-  const handler = awsLambdaFastify(app)
-  await app.ready()
-  return handler
-}
-export const handler = main()
+export const handler = awsLambdaFastify(app)
