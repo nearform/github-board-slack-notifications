5b06625eb5b9e7175f31b31e2037edd3e3426cd8
Kenan Dervisevic <kenan.dervisevic@nearform.com>
2022-08-12 12:38:15 +0200

    update

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -43,7 +43,7 @@ resource "aws_lambda_permission" "lambda_permission" {
 
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
-  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*/github-board-slack-notifications-test"
+  source_arn = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
 }
 
 resource "aws_cloudwatch_log_group" "convert_log_group" {
