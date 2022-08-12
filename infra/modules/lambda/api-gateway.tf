307d6af15a7756ad9313eaceb1ace52b3583e645
Kenan Dervisevic <kenan.dervisevic@nearform.com>
2022-08-12 12:30:33 +0200

    update

--- infra/modules/lambda/api-gateway.tf
+++ infra/modules/lambda/api-gateway.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "webhook"
+  path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
