79e0d220d0f2247e1c05f7df83c840cd2785b006
Kenan Dervisevic <kenan.dervisevic@nearform.com>
2022-08-12 15:04:59 +0200

    update staging naming for api gateway

--- infra/modules/lambda/api-gateway.tf
+++ infra/modules/lambda/api-gateway.tf
@@ -54,12 +54,12 @@ resource "aws_api_gateway_deployment" "apideploy" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
-  stage_name  = "test"
+  stage_name  = var.env
 }
 
 resource "aws_api_gateway_method_settings" "gateway_stage" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
-  stage_name  = "test"
+  stage_name  = var.env
   method_path = "webhook/POST"
 
   settings {
