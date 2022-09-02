6bfc5b8a58cc55c78180cfaadafe28aa9553592b
Marco Ippolito <marcoippolito54@gmail.com>
2022-09-02 09:34:01 +0200

    fix: moved lambda loggin permission under iam

--- infra/modules/lambda/iam.tf
+++ infra/modules/lambda/iam.tf
@@ -14,3 +14,31 @@ resource "aws_iam_role" "lambda_role" {
   name               = "${var.project}-${var.env}-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.arn
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
